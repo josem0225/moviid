88b05ca36047b9e18ff84cd757910ba1b9f8a70c
Jose Miguel Rozo Baez <josemiguelrozobaez@192.168.0.11>
2022-05-31 17:51:53 -0500

    ComponentsCreated

--- src/app/shared/shared-module.module.ts
+++ src/app/shared/shared-module.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HomeComponent } from './components/home/home.component';
+import { IntroductionComponent } from './components/introduction/introduction.component';
+import { DatosComponent } from './components/datos/datos.component';
+import { DispositivoComponent } from './components/dispositivo/dispositivo.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { MoveComponent } from './components/move/move.component';
+import { AliadosComponent } from './components/aliados/aliados.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
+    IntroductionComponent,
+    DatosComponent,
+    DispositivoComponent,
+    CarouselComponent,
+    MoveComponent,
+    AliadosComponent,
+    ContactoComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
